e2e3003c0b0e08d7fa79828de3d60b243296cf45
kalinichev91 <kalinichev67@yandex.ru>
2025-10-31 20:27:00 +0300

    feat: Реализовали функцию пополнения баланса

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ while (true) {
         case '1':
             alert(`Ваш баланс: ${balance} `);
             break;
+        case '2':
+            const accountTo = Number(prompt('Введите сумму для снятия'));
+            if (isNaN(accountTo) || accountTo <=0) {
+                alert('Ошибка: введена некоректная сумма.');
+            }else if (accountTo > balance) {
+                alert('Ошибка: недостаточно средств на счете');
+            }else{
+                balance -= accountTo;
+                alert(`Сгятие успешно. Ваш баланс: ${balance} `);
+                break
+            }
+            case '2':
+            alert(`Ваш баланс: ${balance} `);
+            break;
+        case '2':
+            const accountToDeposit = Number(prompt('Введите сумму для снятия'));
+            if (isNaN(accountToDeposit) || accountToDeposit <=0) {
+                alert('Ошибка: введена неккоректная сумма.');
+            
+            }else{
+                balance =- accountToDeposit;
+                alert(`Счет пополнен. Ваш баланс: ${balance} `);
+                break;
+            }
 
     }
      
